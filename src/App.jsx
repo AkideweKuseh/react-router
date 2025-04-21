7486dde5008fb87e72729c70ef8c64b2256598f7
AkideweKuseh <98606196+AkideweKuseh@users.noreply.github.com>
2025-04-21 09:47:30 +0000

    Section 8 Completed

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+// import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+//layouts
+import RootLayout from "./Layouts/RootLayout";
+import HelpLayout from "./Layouts/HelpLayout";
+import CareersLayout from "./Layouts/CareersLayout";
+
+//pages
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import NotFound from "./Pages/NotFound";
+import Faq from "./Pages/Help/Faq";
+import Contact from "./Pages/Help/Contact";
+import Careers, { careersLoader } from "./Pages/Careers/Careers";
+import CareerDetails, {
+  careerDetailsLoader,
+} from "./Pages/Careers/CareerDetails";
+import CareersError from "./Pages/Careers/CareersError";
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="help" element={<HelpLayout />}>
+          <Route path="faq" element={<Faq />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+        <Route
+          path="careers"
+          element={<CareersLayout />}
+          errorElement={<CareersError />}
+        >
+          <Route index element={<Careers />} loader={careersLoader} />
+          <Route
+            path=":id"
+            element={<CareerDetails />}
+            loader={careerDetailsLoader}
+          />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    )
+  );
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default App;
+
+//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+////////// rgb(225, 0, 0)  OLDER WAY ///////////////////////////////////////////
+// <BrowserRouter>
+//   <header>
+//     <nav>
+//       <h1>Job-Tour</h1>
+//       <NavLink to="/">Home</NavLink>
+//       <NavLink to="/about">About</NavLink>
+//       <NavLink to="/contact">Contact</NavLink>
+//     </nav>
+//   </header>
+//   <main>
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/about" element={<About />} />
+//       <Route path="/contact" element={<Contact />} />
+//       <Route path="*" element={<NotFound />} />
+//     </Routes>
+//   </main>
+// </BrowserRouter>
---
+++ src/Pages/index.js
@@ -0,0 +1,18 @@
+// import Home from "./Home";
+// import About from "./About";
+// import Contact from "./Contact";
+// import NotFound from "./NotFound";
+
+// // Import all your pages here
+// // Add more page imports as needed
+
+// // Export all pages as a single object
+// export const Pages = {
+//   Home,
+//   About,
+//   Contact,
+//   NotFound,
+//   // Add more pages as needed
+// };
+
+// NB: did not work
